87030b587805659c2c9f5a1b0278f7def78ff122
Rovlet <zwolinskajustyna12@gmail.com>
2022-04-17 22:00:24 +0200

    Update parser

--- CNN_LSTM/logs_parser.py
+++ CNN_LSTM/logs_parser.py
@@ -3,18 +3,21 @@ import sys
 from logparser.Drain import Drain
 from settings import *
 
+input_dir = "logs_prepared/"  # The input directory of log file
+output_dir = "logs_parsed/"  # The output directory of parsing results
+log_file_all = "nsmc-kibana_new.txt"  # The input log file name
+log_format = "<Date> <Time> <Content>"
 regex = [
-    r"blk_(|-)[0-9]+",  # block id
-    r"(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)",  # IP
-    r"(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$",  # Numbers
+    r"host=(\[.*])",  # source
+    r"url(=\[.*\])",  # url
+    r"([0-9]*ms)",  # time
+    r"([0-9]*\.[0-9]*B)",  # size
 ]
-st = 0.5
-depth = 4
+st = 0.8
+depth = 9
 
 parser = Drain.LogParser(
-    LOG_FORMAT, indir=LOGS_INPUT_DIR, outdir=LOGS_PARSED_OUTPUT_DIR, depth=depth, st=st, rex=regex
+    log_format, indir=input_dir, outdir=output_dir, depth=depth, st=st, rex=regex
 )
-
-
-def parse_logs(logs_path):
-    parser.parse(logs_path)
+if __name__ == '__main__':
+    parser.parse(log_file_all)

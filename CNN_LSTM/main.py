4ba2c16f670c12474bab3f423bc138e3bf0e602c
Paweł Ptak <pawel.ptak@atos.net>
2022-04-19 11:04:58 +0200

    add column standarization

--- CNN_LSTM/main.py
+++ CNN_LSTM/main.py
@@ -5,7 +5,7 @@ from preparing.sliding_window_processor import prepare_dataframe, FeatureExtract
 from preparing.prepare_dataset import prepare_custom_datasets, add_padding
 from system_log_parser import logs_parser
 from settings import *
-from preparing import prepare_data_in_parsed_file, prepare_model
+from preparing import prepare_data_in_parsed_file, prepare_model, standarize
 from evaluate_after_training.plots import *
 torch.manual_seed(RANDOM_SEED)
 if torch.cuda.is_available():
@@ -25,3 +25,4 @@ if __name__ == '__main__':
         # collecting events
         dataframe = prepare_dataframe(x, re_pat)
         dataframe.to_csv("{}events.csv".format(LOGS_PARSED_OUTPUT_DIR), index=False)
+        standarize.standarize_df("{}events.csv".format(LOGS_PARSED_OUTPUT_DIR), columns_to_standarize=['url_malicious_score', 'time [ms]', 'size [B]'])
